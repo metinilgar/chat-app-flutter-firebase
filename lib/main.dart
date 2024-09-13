1b330159861d82c33c621a0b8e2479a5cff60b24
metinilgar <metinilgarmutlu@gmail.com>
2024-09-13 20:10:38 +0300

    Add firebase to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:chat_app_flutter_firebase/firebase_options.dart';
 import 'package:chat_app_flutter_firebase/src/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const ProviderScope(child: MainApp()));
 }
